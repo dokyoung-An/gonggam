8f91935ea8f9341c932873bd1db4dff49f2222e0
Dokyung An <tea-co@naver.com>
2025-04-28 12:21:09 +0900

    수정

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.1.8/20986
 // Filename: ??? ??? ??.ggsk
-// Generated 2025-04-28T11:54:32
+// Generated 2025-04-28T11:57:28
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_aptInfo', 2, false, { ignoreInState: 0  });
